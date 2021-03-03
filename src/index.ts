f745e406ee4f0b00d67844b2920f8fa96a07f8c1
MatAmo93 <79974907+MatAmo93@users.noreply.github.com>
2021-03-03 19:44:08 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,23 +15,23 @@ async function Test() {
   balance = await exchange.balance("BTC");
   console.log("BTC", "Balance", balance, "Price", price);
 
-  price = await exchange.price("ETH/EUR");
-  balance = await exchange.balance("ETH");
-  console.log("ETH", "Balance", balance, "Price", price);
+  price = await exchange.price("ADA/EUR");
+  balance = await exchange.balance("ADA");
+  console.log("ADA", "Balance", balance, "Price", price);
 
   balance = await exchange.balance("EUR");
   console.log("EUR", "Balance", balance);
 
   console.log("TRADE.");
   // ==
-  await exchange.createOrder("BUY", "ETH/EUR", AppConfig.ORDER_AMOUNT); // amount in quote currency
+  await exchange.createOrder("BUY", "ADA/EUR", AppConfig.ORDER_AMOUNT); // amount in quote currency
 
   console.log("WITHDRAW.");
   // ==
   // console.log("ETH", AppConfig.WALLET_ADDRESS_ETH, AppConfig.WALLET_DESCRIPTION_ETH, 0.1);
   // await exchange.withdraw("ETH", AppConfig.WALLET_ADDRESS_ETH, 0.1, AppConfig.WALLET_DESCRIPTION_ETH);
-  console.log("ETH", AppConfig.WALLET_ADDRESS_ETH, AppConfig.WALLET_DESCRIPTION_ETH, "ALL");
-  await exchange.withdraw("ETH", AppConfig.WALLET_ADDRESS_ETH, undefined, AppConfig.WALLET_DESCRIPTION_ETH);
+  console.log("ADA", AppConfig.WALLET_ADDRESS_ADA, AppConfig.WALLET_DESCRIPTION_ETH, "ALL");
+  await exchange.withdraw("ADA", AppConfig.WALLET_ADDRESS_ADA, undefined, AppConfig.WALLET_DESCRIPTION_ADA);
 
   console.log("DONE.");
 }
